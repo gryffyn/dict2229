9a6e9b34399ec82fff8ea6448665c213f5eea24a
gryffyn <me@neveris.one>
2021-07-29 07:49:55 +0100

    updated gitignore
    Signed-off-by: gryffyn <me@neveris.one>

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-glob="build/app/outputs/apk/release/app-*release.apk"
-flutter build apk --split-per-abi --bundle-sksl-path flutter_01.sksl.json --split-debug-info=debug
-for f in $glob
-do
-  sha256sum $f > $f.sha256
-  scp $f srv:/srv/files/projects/dict2229/.
-  scp $f srv:/srv/files/projects/dict2229/.
-done
